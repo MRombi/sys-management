bb6b245bead7d921eda46ab2cb9d08ea66e0b4e8
michael <michael@michaels-MacBook-Pro.local>
2023-10-16 12:33:01 +0100

    add the topbar component

---
+++ src/Types.ts
@@ -0,0 +1,68 @@
+export interface Colour {
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+  }
+  
+  export interface CustomTheme {
+    grey: Colour;
+    primary: Colour;
+    greenAccent: Colour;
+    redAccent: Colour;
+    blueAccent: Colour;
+  }
+  
+  export interface Theme {
+    palette: {
+      mode: string;
+      primary: {
+        main: string;
+      };
+      secondary: {
+        main: string;
+      };
+      neutral: {
+        dark: string;
+        main: string;
+        light: string;
+      };
+      background: {
+        default: string;
+      };
+    };
+    typography: {
+      fontFamily: string;
+      fontSize: number;
+      h1: {
+        fontFamily: string;
+        fontSize: number;
+      };
+      h2: {
+        fontFamily: string;
+        fontSize: number;
+      };
+      h3: {
+        fontFamily: string;
+        fontSize: number;
+      };
+      h4: {
+        fontFamily: string;
+        fontSize: number;
+      };
+      h5: {
+        fontFamily: string;
+        fontSize: number;
+      };
+      h6: {
+        fontFamily: string;
+        fontSize: number;
+      };
+    };
+    custom: CustomTheme;
+  }
